30a656325368ca5fc9158593a5dc86be50d0d969
level237 <Bramslevel129@gmail.com>
2024-03-31 09:04:45 +0100

    register init

--- actions/auth/register.ts
+++ actions/auth/register.ts
@@ -1,11 +1,25 @@
+'use server'
 import { signIn } from "@/auth";
+import { db } from "@/db";
 
 export async function register(
 formData:FormData
 ){
 try {
-    //await signIn('credentials',formData)
-    console.log(formData);
+  const email=formData.get('email') as string
+  const phone=formData.get('phone') as string
+  const password=formData.get('password') as string
+  const name=formData.get('name') as string
+ 
+   await db.user.create({
+     data:{
+      name,
+      email,
+      phone,
+      password
+     }
+    })
+   
 } catch (error) {
     
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,4 @@
+import { register } from "@/actions/auth/register"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
@@ -8,10 +9,14 @@ export default async function RegisterPage(){
         <>
         <section className="container mt-36 flex items-center flex-col justify-center">
             <h1 className="text-2xl font-bold text-center">Register</h1>
-<Input type="email" placeholder="Enter your email" className="w-96 mt-5"/>
-<Input type="password" placeholder="Enter your password" className="w-96 mt-5"/>
+            <form action={register}>
+                <Input type="text" name="name" placeholder="Enter your Name"/>
+<Input type="email" name="email" placeholder="Enter your email" className="w-96 mt-5"/>
+<Input type="text" name="phone" placeholder="Enter your phone number" className="w-96 mt-5"/>
+<Input type="password" name="password" placeholder="Enter your password" className="w-96 mt-5"/>
 
 <Button type="submit" className="mt-5">Register</Button>
+</form>
         </section>
        
         </>
