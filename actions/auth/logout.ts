8e3b0e6ee36b2c8b4532b7a128483aad5af6a3a5
level237 <Bramslevel129@gmail.com>
2024-04-01 09:31:13 +0100

    logout in action

--- actions/auth/logout.ts
+++ actions/auth/logout.ts
@@ -1,12 +1,16 @@
 'use server'
 import * as auth from "@/auth"
-
+import { NextRequest, NextResponse } from 'next/server'
 import { redirect } from "next/navigation";
 
-export async function logout(){
+
+export async function logout(request:NextRequest){
     
     await new Promise((resolve)=>setTimeout(resolve,2500))
-    const logout=auth.signOut()
-    redirect('/')
+    const logout=auth.signOut({redirect:false}).then(()=>{
+        redirect('/')
+    })
+    //return NextResponse.redirect(new URL('/'))
+    console.log(request);
     return logout;
 }
