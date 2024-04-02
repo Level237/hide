ba27760c929a70fd15596d550e93fa6feed153e7
level237 <Bramslevel129@gmail.com>
2024-04-02 07:46:22 +0100

    login view

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,15 +8,12 @@ import { db } from "@/db"
 
 export default async function LoginPage(){
 
-  const users=await db.user.findMany()
+ 
     return (
         <>
-        <section className="container mt-36 flex items-center flex-col justify-center">
-            <h1 className="text-2xl font-bold text-center">Login</h1>
-            {users.map(u=>u.email)}
+
    <LoginForm/>
-        </section>
-       
+
         </>
     )
 } 
--- components/common/FormBtn.tsx
+++ components/common/FormBtn.tsx
@@ -3,22 +3,33 @@ import React from 'react'
 import { Button } from '../ui/button'
 import { useFormStatus } from 'react-dom'
 import { Loader2 } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
-interface FormButtonProps{
-  children:React.ReactNode
-}
-export default function FormBtn({children}:FormButtonProps) {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  }
+const FormBtn=React.forwardRef<HTMLInputElement,InputProps>(({ className,children }) =>{
   const {pending}=useFormStatus()
 
   return (
     <div>
       {
-        !pending ?  <Button  type="submit" className="mt-5"  >{children}</Button> : <Button className="mt-5" disabled>
+        !pending ?  <Button  type="submit"  className={cn(
+          "mt-5",
+          className
+        )} variant="default"  >{children}</Button> : <Button className={cn(
+          "mt-5",
+          className
+        )} disabled>
         <Loader2 className="mr-2 h-4 w-4 animate-spin" />
         Please wait
       </Button>
       }
-     
+ 
     </div>
   )
-}
+})
+
+FormBtn.displayName = "FormBtn"
+
+export { FormBtn }
