be3e5ee898f5850e4689a1afcf91bf50f1404a36
level237 <Bramslevel129@gmail.com>
2024-04-03 21:56:50 +0100

    hide section and recent activities

--- app/profile/[name]/page.tsx
+++ app/profile/[name]/page.tsx
@@ -3,6 +3,7 @@ import { auth } from '@/auth';
 import {FormBtn} from '@/components/common/FormBtn';
 import HeaderAuth from '@/components/header/HeaderAuth';
 import { HomeComponent } from '@/components/Home/Home';
+import NewHideSection from '@/components/profile/NewHideSection';
 import ProfileSection from '@/components/profile/ProfileSection';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
@@ -10,7 +11,7 @@ import { Button } from '@/components/ui/button';
 import Header from '@/components/ui/Header';
 import getUserByName from '@/db/queries/users/getUserByName';
 import { Eye, Plus } from 'lucide-react';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 import React from 'react'
 
 interface ProfileShowPageProps{
@@ -24,6 +25,8 @@ export default async function ProfilePage({params}:ProfileShowPageProps) {
   const user=await getUserByName(name);
  if(!user){
   notFound()
+ }else if(session?.user?.name!==name){
+  redirect('/')
  }
   const submenuNav = [
     { title: "Overview", path: "javascript:void(0)" },
@@ -61,29 +64,16 @@ export default async function ProfilePage({params}:ProfileShowPageProps) {
     <HomeComponent>
    <section  className='flex mt-10 mx-10'>
     <ProfileSection/>
-    <div className=' w-full py-12 rounded-lg pl-12 pr-12 max-h-96 border-[#00000013] border-[0.12rem]' >
+    <div className=' w-full py-12 rounded-lg pl-12 pr-12  border-[#00000013] border-[0.12rem]' >
       <h2 className='text-center text-black font-bold text-2xl mb-5'>Welcome in Hide,{session?.user?.name}</h2>
-<div className='grid grid-cols-3 gap-5'>
-  <div className='flex flex-col'>
-  <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#0000001c] duration-150  border-[#0000002f] border-[0.12rem]'>
-<Plus/>
-</div>
-<h2 className='text-center mt-3 text-primary font-bold'>New Post</h2>
-  </div>
-  <div className='flex flex-col'>
-  <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#0000001c] duration-150  border-[#0000002f] border-[0.12rem]'>
-<Plus/>
-</div>
-<h2 className='text-center mt-3 text-primary font-bold'>New Notes</h2>
-  </div>
-  <div className='flex flex-col'>
-  <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#0000001c] duration-150  border-[#0000002f] border-[0.12rem]'>
-<Plus/>
-</div>
-<h2 className='text-center mt-3 text-primary font-bold'>New Hide response</h2>
-  </div>
-
-</div>
+      <NewHideSection/>
+      <section className='mt-20'>
+      <h2 className=' text-black font-bold text-xl mb-5'>Recents Activities</h2>
+      <div className=' py-[5rem] flex items-center justify-center rounded-lg   hover:text-black   border-[#0000002f] border-[0.12rem]'>
+                    No recents activities
+      </div>
+      </section>
+      
     </div>
    </section>
     </HomeComponent>
---
+++ components/profile/NewHideSection.tsx
@@ -0,0 +1,30 @@
+import { Plus } from 'lucide-react'
+import React from 'react'
+
+export default function NewHideSection() {
+  return (
+    <div>
+      <div className='grid grid-cols-3 gap-5'>
+  <div className='flex flex-col'>
+  <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#0000001c] duration-150  border-[#0000002f] border-[0.12rem]'>
+<Plus/>
+</div>
+<h2 className='text-center mt-3 text-primary font-bold'>New Post</h2>
+  </div>
+  <div className='flex flex-col'>
+  <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#0000001c] duration-150  border-[#0000002f] border-[0.12rem]'>
+<Plus/>
+</div>
+<h2 className='text-center mt-3 text-primary font-bold'>New Notes</h2>
+  </div>
+  <div className='flex flex-col'>
+  <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#0000001c] duration-150  border-[#0000002f] border-[0.12rem]'>
+<Plus/>
+</div>
+<h2 className='text-center mt-3 text-primary font-bold'>New Hide response</h2>
+  </div>
+
+</div>
+    </div>
+  )
+}
