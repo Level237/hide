48c5a23badb9b82867cf1f323d33130a56420a7a
level237 <Bramslevel129@gmail.com>
2024-04-03 10:23:16 +0100

    profile section:added information current user

--- components/profile/ProfileSection.tsx
+++ components/profile/ProfileSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Avatar, AvatarFallback } from '../ui/avatar'
 import { Button } from '../ui/button'
-import { Eye } from 'lucide-react'
+import { Eye, EyeOff, NotepadText } from 'lucide-react'
 
 export default function ProfileSection() {
   return (
@@ -13,12 +13,23 @@ export default function ProfileSection() {
 <AvatarFallback className='bg-slate-800 text-white'>CN</AvatarFallback>
 </Avatar>
       </div>
-    <div className='flex items-center'>
-      <h2 className='font-bold  text-2xl mx-6'>Level  <span className="font-thin text-[1.2rem] text-gray-500"> | Anonymous</span></h2>
+    <div className='flex flex-row items-center'>
+      <h2 className='font-bold  text-2xl mx-6'>Level  <span className="font-thin text-[1.2rem] text-gray-500"> | Anonymous </span></h2>
     </div>
 
     <div className='mx-5'>
       <Button className='px-[5rem] flex justify-between gap-2'>New Hide<Eye/></Button>
+    </div>
+    <div className='flex gap-2 justify-between'>
+        <div className='flex items-center'>
+        <NotepadText className='w-5'/><span className="text-gray-500"><span className='font-bold text-black'>0</span> notes</span>
+        </div>
+        <div className='flex items-center'>
+        <EyeOff className='w-5'/><span className="text-gray-500"><span className='font-bold text-black'>0</span> hide</span>
+        </div>
+        <div className='flex items-center'>
+        <EyeOff className='w-5'/><span className="text-gray-500"><span className='font-bold text-black'>0</span> hide</span>
+        </div>
     </div>
     </div>
 </div>
