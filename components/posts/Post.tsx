b9563cd784cf7c3e2b523c5151cdfa9e8cff8ed5
level237 <Bramslevel129@gmail.com>
2024-04-05 15:41:07 +0100

    switch bgcolor Post

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { Textarea } from '../ui/textarea'
 import { Palette } from 'lucide-react'
 import { Button } from '../ui/button'
 
 export default function Post() {
+
+    const [bgPost,setBgPost]=useState('#2dac5c')
+
+    const changeBgHandler=(color:string)=>{
+        setBgPost(color)
+    }
   return (
     <div>
       <section className='container mt-[4rem] '>
@@ -11,18 +19,18 @@ export default function Post() {
       <form action="" className='mt-5'>
        
         <div className='mt-4 flex justify-center'>
-        <Textarea placeholder="Type your message here." className='font-bold mx-36 overflow-y-hidden placeholder:text-gray-300 placeholder:flex py-[5rem] focus:border-none text-center h-[15rem] text-white flex justify-center text-xl bg-primary' />
+        <Textarea placeholder="Type your message here." className={`font-bold mx-36 overflow-y-hidden placeholder:text-gray-300  py-[5rem]  text-center h-[15rem] text-white flex justify-center text-xl bg-[${bgPost}]`} />
         
         </div>
         <section className=' mx-36 flex items-center justify-between'>
         <div className='mt-5  w-[14rem] grid grid-cols-4 gap-4'>
-            <div className='h-12 cursor-pointer bg-primary rounded-sm'>
+            <div  onClick={()=>changeBgHandler("#2cac5c")} className='h-12 cursor-pointer bg-primary rounded-sm'>
 
             </div>
-            <div className='h-12 cursor-pointer bg-[#ff0000] rounded-sm'>
+            <div onClick={()=>changeBgHandler("#ff0000")} className='h-12 cursor-pointer bg-[#ff0000] rounded-sm'>
 
             </div>
-            <div className='h-12 cursor-pointer bg-[#000] rounded-sm'>
+            <div onClick={()=>changeBgHandler("#000")} className='h-12 cursor-pointer bg-[#000] rounded-sm'>
 
             </div>
             <div className='h-12 flex justify-center items-center cursor-pointer bg-gray-200 border-[0.03px] border-primary rounded-sm'>
