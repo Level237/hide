5ae6d6530afc6b4b5db5657bad709eb37398bae8
level237 <Bramslevel129@gmail.com>
2024-04-05 18:46:22 +0100

    back previous route

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css"
 import Providers from "./provider";
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
       
       <body className={inter.className}>
       <Providers>
-        
+      <NextTopLoader
+      color="#2cac5c"
+      />
       {children}
         </Providers>
         </body>
