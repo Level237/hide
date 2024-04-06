2035b90cdc4d4290e98e448bc95ef4cf13cc4868
level237 <Bramslevel129@gmail.com>
2024-04-06 07:23:48 +0100

    create post

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -6,6 +6,9 @@ import { MoveLeft, Palette, Save } from 'lucide-react'
 import { Button } from '../ui/button'
 import { PickerExample } from '../PicExample'
 import { useRouter } from 'next/navigation'
+import { Input } from '../ui/input'
+import CreatePost from '@/actions/post/create'
+
 
 
 export default function Post() {
@@ -27,12 +30,13 @@ export default function Post() {
       </section>
       <section className='container mt-[4rem] '>
       <h2 className='text-center font-bold text-2xl'>New Post</h2>
-      <form action="" className='mt-5'>
+      <form action={CreatePost} className='mt-5'>
        
         <div className='mt-4 flex justify-center'>
-        <Textarea placeholder="Type your message here." className={`font-bold mx-36 overflow-y-hidden placeholder:text-gray-300  py-[6rem]  text-center h-[15rem] text-white flex justify-center text-xl`} style={{ background:`${bgPost}` }} />
-        
+        <Textarea name='content' placeholder="Type your message here." className={`font-bold mx-36 overflow-y-hidden placeholder:text-gray-300  py-[6rem]  text-center h-[15rem] text-white flex justify-center text-xl`} style={{ background:`${bgPost}` }} />
+     
         </div>
+        <Input type="hidden" name='color' value={bgPost} />
         <section className=' mx-36 flex items-center justify-between'>
         <div className='mt-5  w-[14rem] grid grid-cols-4 gap-4'>
             <div  onClick={()=>changeBgHandler("#2cac5c")} className='h-12 cursor-pointer bg-primary rounded-sm'>
@@ -52,7 +56,7 @@ export default function Post() {
           
         </div>
         <div>
-            <Button>Publier</Button>
+            <Button type='submit'>Publier</Button>
         </div>
         </section>
        
