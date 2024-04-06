cd65eb7a5a4bfac63d7a7c49092bae24919d22fa
level237 <Bramslevel129@gmail.com>
2024-04-06 11:47:56 +0100

    fetch post current user

--- components/posts/postList.tsx
+++ components/posts/postList.tsx
@@ -9,11 +9,11 @@ export default async function PostList() {
   return (
     <div className='grid grid-cols-3 gap-5'>
         {posts.map((post)=><div key={post.id}>
-                <Link href="">
+                <Link href={`/posts/${post.id}`}>
     
     <div className='flex flex-col'>
-    <div className=' py-[5rem] flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#00000013] duration-150  border-[#0000002f] border-[0.12rem]'>
-    
+    <div style={{ background:`${post.bgColor}` }} className={`relative py-[5rem]  flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#00000013] duration-150  border-[#0000002f] border-[0.12rem]`}>
+    <p className='text-center text-white p-3'>{post.content}</p>
     </div>
     <h2 className='text-center mt-3 text-primary font-bold'>New Post</h2>
     </div>
