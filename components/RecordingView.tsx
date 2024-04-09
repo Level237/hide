ff599e126ba7ffa6f82833d3d4881abdfc8ef2ba
level237 <Bramslevel129@gmail.com>
2024-04-09 21:04:47 +0100

    update

--- components/RecordingView.tsx
+++ components/RecordingView.tsx
@@ -1,58 +1,107 @@
-import { useEffect, useRef, useState } from "react";
-import Link from "next/link";
+import React, { useRef, useState, useEffect } from 'react';
+import WaveSurfer from 'wavesurfer.js';
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js';
+import RecordPlugin from 'wavesurfer.js/dist/plugins/record.js';
+import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.js';
 
-const formWaveSurferOptions = (ref:any) => ({
-  container: ref,
-  waveColor: "#eee",
-  progressColor: "#2cac5c",
-  cursorColor: "OrangeRed",
-  barWidth: 3,
-  barRadius: 3,
-  responsive: true,
-  height: 150,
-  normalize: true,
-  partialRender: true
-});
-
-export default function AudioVisualizer() {
-  const waveformRef = useRef(null);
+const AudioVisualizer = () => {
+  const waveformRef = useRef<any>(null);
   const wavesurfer = useRef<any>(null);
-  const [playing, setPlaying] = useState(false);
-
-  const url =
-    "hide.mp3";
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioURL, setAudioURL] = useState('');
+  const mediaRecorder = useRef<any>(null);
 
   useEffect(() => {
-    create();
+    if (waveformRef.current) {
+      wavesurfer.current = WaveSurfer.create({
+        container: waveformRef.current,
+        waveColor: 'violet',
+        progressColor: 'purple',
+        backend: 'WebAudio',
+        height: 100,
+        normalize: true,
+        hideScrollbar: true,
+        plugins: [
+          RegionsPlugin.create(),
+          RecordPlugin.create({}),
+          TimelinePlugin.create({
+            container: '#timeline',
+          }),
+        ],
+      });
 
-    return () => {
-      if (wavesurfer.current) {
-        wavesurfer.current.destroy();
-      }
-    };
+      wavesurfer.current.on('audioprocess', (time:number) => {
+        if (isRecording) {
+          const duration = wavesurfer.current.getDuration();
+          wavesurfer.current.clearRegions();
+          wavesurfer.current.addRegion({
+            start: 0,
+            end: time,
+            color: 'rgba(0, 123, 255, 0.1)',
+          });
+        }
+      });
+    }
+
+    return () => wavesurfer.current?.destroy();
   }, []);
 
-  const create = async () => {
-    const WaveSurfer = (await import("wavesurfer.js")).default;
+  const startRecording = async () => {
+    if (!navigator.mediaDevices) {
+      console.error('Enregistrement non pris en charge par ce navigateur');
+      return;
+    }
 
-    const options = formWaveSurferOptions(waveformRef.current);
-    wavesurfer.current = WaveSurfer.create(options);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder.current = new MediaRecorder(stream);
 
-    wavesurfer.current.load(url);
+    mediaRecorder.current.onstart = () => {
+      setIsRecording(true);
+      
+    };
+
+    mediaRecorder.current.onstop = () => {
+      setIsRecording(false);
+    };
+
+    mediaRecorder.current.ondataavailable = (event:any) => {
+      const audioBlob = new Blob([event.data], { type: 'audio/wav' });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudioURL(audioUrl);
+      wavesurfer.current.load(audioUrl);
+    };
+
+    mediaRecorder.current.start();
   };
 
-  const handlePlayPause = () => {
-    setPlaying(!playing);
-    wavesurfer.current.playPause();
+  const stopRecording = () => {
+    mediaRecorder.current?.stop();
+  };
+
+  const handleRecordClick = () => {
+    if (isRecording) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  };
+
+  const handlePlay = () => {
+    wavesurfer.current?.playPause();
   };
 
   return (
     <div>
-      <div id="waveform" ref={waveformRef} />
-      <div className="controls">
-        <div onClick={handlePlayPause}>{!playing ? "Слушать" : "Пауза"}</div>
-      </div>
-      <Link href={{ pathname: "/about" }}>xxx</Link>
+      <div id='timeline'></div>
+      <button onClick={handleRecordClick}>
+        {isRecording ? 'Arrêter' : 'Enregistrer'}
+      </button>
+      <button onClick={handlePlay} disabled={!audioURL}>
+        Lire/Pause
+      </button>
+      <div ref={waveformRef} className='waveform'></div>
     </div>
   );
-}
+};
+
+export default AudioVisualizer;
