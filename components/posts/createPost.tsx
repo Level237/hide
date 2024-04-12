a9684d904fd1e0480891b58bd9dcab7a01297287
level237 <Bramslevel129@gmail.com>
2024-04-12 13:43:24 +0100

    design update:icons multimedia

--- components/posts/createPost.tsx
+++ components/posts/createPost.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import {motion} from "framer-motion"
 import React, { useState } from 'react'
 import { Textarea } from '../ui/textarea'
-import { MoveLeft, Palette, Save, Send } from 'lucide-react'
+import { BookHeart, MicOff, MicVocalIcon, MoveLeft, Palette, Save, Send, VenetianMaskIcon, Waves, X } from 'lucide-react'
 import { Button } from '../ui/button'
 import { PickerExample } from '../PicExample'
 import { useRouter } from 'next/navigation'
@@ -20,7 +21,11 @@ export default function Post() {
         console.log(color);
     }
   return (
-    <div className='relative h-[100vh]'>
+
+    <motion.div 
+    initial={{ scale:0,opacity:0 }}
+    animate={{ scale:1,opacity:1 }}
+    className='relative h-[100vh]'>
       
       <section className='container'>
       <h2 className='text-center font-bold text-2xl'>New Post</h2>
@@ -28,32 +33,52 @@ export default function Post() {
        
         <div className='mt-4 flex justify-center'>
         <Textarea name='content' placeholder="Type your message here." className={`font-bold mx-36 overflow-y-hidden placeholder:text-gray-300   absolute  top-0 h-full py-60  text-center  text-white flex justify-center  text-xl`} style={{ background:`${bgPost}` }} />
-        <section className='absolute w-full top-1  flex   items-center justify-between '>
-          
-        <Button onClick={()=>router.back()} className='bg-transparent text-primary mx-12 hover:bg-transparent'><MoveLeft/>Back</Button>
+        <section className='absolute w-full top-1 mx-5  flex   items-center justify-between '>
+          <X onClick={()=>router.back()} className='text-white w-[2rem] mx-5 cursor-pointer h-[2rem] '/>
+        
         <div className='flex items-center gap-2'>
         <Button className='mx-12'>Publier <Save className='w-5 ml-2'/></Button>
         </div>
-        
-      </section>
-        </div>
-        <Input type="hidden" name='color' value={bgPost} />
-        <section className='  bottom-0  w-[95vw] my-5 flex items-center justify-between absolute'>
-        <div className='mt-5  w-[14rem] grid grid-cols-4 gap-4 z-20'>
-            <div  onClick={()=>changeBgHandler("#2cac5c")} className='h-12 cursor-pointer bg-primary rounded-sm'>
+        <motion.div className="p-2 rounded-md left-[-11rem] bg-white absolute flex justify-center top-[35vh]"
+         
+         whileHover={{ x:175}}
+         transition={{ 
+          duration:1
+          }}
+         >
+        <div className='  w-[14rem] grid grid-cols-4 gap-4 z-20'>
+            <div  onClick={()=>changeBgHandler("#2cac5c")} className='h-10 cursor-pointer bg-primary rounded-sm'>
 
             </div>
-            <div onClick={()=>changeBgHandler("#ff0000")} className='h-12 cursor-pointer bg-[#ff0000] rounded-sm'>
+            <div onClick={()=>changeBgHandler("#ff0000")} className='h-10 cursor-pointer bg-[#ff0000] rounded-sm'>
 
             </div>
-            <div onClick={()=>changeBgHandler("#000")} className='h-12 cursor-pointer bg-[#000] rounded-sm'>
+            <div onClick={()=>changeBgHandler("#000")} className='h-10 cursor-pointer bg-[#000] rounded-sm'>
 
             </div>
-            <div className='h-12 relative flex flex-row justify-center items-center cursor-pointer bg-gray-200 border-[0.03px] border-primary rounded-sm'>
+            <div style={{ background:`${bgPost}` }} className='h-10 relative flex flex-row justify-center items-center cursor-pointer   rounded-sm'>
                 <Palette/>
                 <PickerExample changeBgHandler={changeBgHandler}/>
                 
             </div>
+          
+        </div>
+        </motion.div>
+      </section>
+        </div>
+        <Input type="hidden" name='color' value={bgPost} />
+        <section className='  bottom-0  w-[95vw] my-5 flex items-center justify-between absolute'>
+        <div className='mt-5 bg-white py-3 px-6 w-[14rem] grid grid-cols-3 gap-4 z-20'>
+            <div   className='h-12 cursor-pointer  rounded-sm'>
+            <VenetianMaskIcon className="h-8 w-8"/>
+            </div>
+            <div  className='h-12 cursor-pointer  rounded-sm'>
+            <MicVocalIcon className="h-8 w-8"/>
+            </div>
+            <div  className='h-12 cursor-pointer  rounded-sm'>
+            <BookHeart className="h-8 w-8"/>
+            </div>
+            
           
         </div>
         <div className='z-10'>
@@ -63,7 +88,7 @@ export default function Post() {
        
       </form>
     </section>
-    </div>
+    </motion.div>
   )
 }
 
