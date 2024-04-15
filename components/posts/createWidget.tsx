0a5abb1161977dd8bbd64fa3595ae314d543e718
level237 <Bramslevel129@gmail.com>
2024-04-15 07:52:11 +0100

    update view ui:create post

--- components/posts/createWidget.tsx
+++ components/posts/createWidget.tsx
@@ -1,26 +1,37 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { Textarea } from '../ui/textarea'
 import { CirclePlus, Mic, Send } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 
 export default function CreateWidget() {
+
+    const [bgPost,setBgPost]=useState<string>("")
+    const [color,setColor]=useState<string>("")
+
+    const changeBgHandler=(color:string)=>{
+        setBgPost(color)
+        setColor("#fff")
+        console.log(color);
+    }
   return (
     <div className="bg-white relative rounded-3xl w-[90vw] py-[5rem] mt-3 flex justify-center">
   <div className="w-[90%] relative">
-  <Textarea name='content' placeholder="Type your message here." className={`font-bold w-full py-[5rem] flex justify-center relative overflow-y-hidden border-[#00000041] rounded-2xl placeholder:text-gray-300         text-black  text-xl`}  />
+  <Textarea style={{ background:`${bgPost}`,color:`${color}` }} name='content' placeholder="Type your message here." className={`font-bold w-full py-[5rem] flex justify-center relative overflow-y-hidden border-[#00000041] rounded-2xl placeholder:text-gray-300         text-black  text-xl`}  />
   <div className="flex justify-end absolute bottom-4 w-[100vw]">
   <div className="flex flex-1  items-center gap-3 mx-4 ">
-      <div className="bg-black p-4 rounded-full cursor-pointer">
+      <div onClick={()=>changeBgHandler("#000")}  className="bg-black p-4 rounded-full cursor-pointer">
 
       </div>
-      <div className="bg-primary p-4 rounded-full cursor-pointer">
+      <div onClick={()=>changeBgHandler("#2cac5c")} className="bg-primary p-4 rounded-full cursor-pointer">
 
       </div>
-      <div className=" p-4 rounded-full cursor-pointer" style={{ background:"linear-gradient(to top left,#fcc5e4,#fda34b,#ff7882,#c8699e,#7046aa,#0c1db8,#020f75)" }}>
+      <div className=" p-4 rounded-full cursor-pointer" onClick={()=>changeBgHandler("linear-gradient(to top left,#fcc5e4,#fda34b,#ff7882,#c8699e,#7046aa,#0c1db8,#020f75)")} style={{ background:"linear-gradient(to top left,#fcc5e4,#fda34b,#ff7882,#c8699e,#7046aa,#0c1db8,#020f75)" }}>
 
       </div>
-      <div className=" p-4 rounded-full cursor-pointer" style={{ background:"linear-gradient(to top left,#000000,#434343)" }}>
+      <div onClick={()=>changeBgHandler("linear-gradient(to top left,#000000,#434343)")} className=" p-4 rounded-full cursor-pointer" style={{ background:"linear-gradient(to top left,#000000,#434343)" }}>
 
       </div>
       <div className="cursor-pointer" >
