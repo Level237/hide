9420c0fd25a2b32f5c5a26cd41f862b3b65e92c7
level237 <Bramslevel129@gmail.com>
2024-04-16 11:14:18 +0100

    update post profile

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Sidebar from "@/components/sidebar";
 import HomeAuth from "@/components/Home/HomeAuth";
 import HeaderProfile from "@/components/header/HeaderProfile";
 import { Textarea } from "@/components/ui/textarea";
-import { CirclePlus, LucideMessageSquare, Mic, Send, Share, ThumbsDown, ThumbsUp } from "lucide-react";
+import { CirclePlus, LucideMessageSquare, Mic, MoreHorizontal, Send, Share, ThumbsDown, ThumbsUp } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import CreateWidget from "@/components/posts/createWidget";
@@ -42,19 +42,26 @@ zz
 <section>
 <h2 className="font-bold text-gray-700 text-xl">Mon Mur</h2>
 <div className="mt-5 flex flex-col gap-3">
-  <div className="bg-white px-36 py-12">
-  <div className="  p-10 px-8 rounded-2xl " style={{ background:"linear-gradient(to top left,#000000,#434343)" }}>
-        <div className="flex justify-between w-[5rem] gap-3 ">
+  <div className="bg-white px-12 py-12 rounded-3xl">
+    <div className="flex justify-between">
+    <div className="flex justify-between w-[5rem] gap-3 ">
         <Avatar className='cursor-pointer'>
                     <AvatarImage src="/hidd.jpg" alt="@shadcn" />
             <AvatarFallback>CN</AvatarFallback>
             </Avatar>
     
             <div className="flex flex-col">
-              <h2 className="font-bold text-md " style={{ color:"white" }}>#uizog234</h2>
-              <span className="text-sm text-gray-500 text-primary">1h ago</span>
+              <h2 className="font-bold text-md " style={{ color:"black" }}>#uizog234</h2>
+              <span className="text-sm text-gray-500 ">1h ago</span>
             </div>
         </div>
+        <div className="">
+       <MoreHorizontal/>
+        </div>
+    </div>
+  
+  <div className="mt-5 h-96 flex justify-center items-center  p-10 px-8 rounded-2xl " style={{ background:"linear-gradient(to top left,#000000,#434343)" }}>
+        
         <div className="mt-5">
           <h2 className="text-3xl font-bold" style={{ color:"white" }}>-How create post in Hide ,Please..I have forget that...</h2>
         </div>
