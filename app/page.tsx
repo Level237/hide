06065b97c5dad3f90324f152cb8ea701c62cc9c9
level237 <Bramslevel129@gmail.com>
2024-04-18 11:34:20 +0100

    post list components

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ import CreateWidget from "@/components/posts/createWidget";
 import HeaderAuth from "@/components/header/HeaderAuth";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import RecentPost from '../components/posts/recentPost';
+import PostList from "@/components/posts/postList";
 
 export default function Home() {
   return (
@@ -41,125 +42,7 @@ SideBar Section
 </div>
 <section>
 <h2 className="font-bold text-gray-700 text-xl">Mon Mur</h2>
-<div className="mt-5 flex flex-col gap-3">
-  <div className="bg-white px-12 py-12 mb-6 rounded-3xl">
-    <div className="flex justify-between">
-    <div className="flex justify-between w-[5rem] gap-3 ">
-        <Avatar className='cursor-pointer'>
-                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-    
-            <div className="flex flex-col">
-              <h2 className="font-bold text-md " style={{ color:"black" }}>#uizog234</h2>
-              <span className="text-sm text-gray-500 ">1h ago</span>
-            </div>
-        </div>
-        <div className="">
-       <MoreHorizontal/>
-        </div>
-    </div>
-  
-  <div className="mt-5 h-96 flex justify-center items-center  p-10 px-8 rounded-2xl " style={{ background:"linear-gradient(to top left,#000000,#434343)" }}>
-        
-        <div className="mt-5">
-          <h2 className="text-3xl font-bold" style={{ color:"white" }}>-How create post in Hide ,Please..I have forget that...</h2>
-        </div>
-        
-    </div>
-    
-    <div className="flex mt-5 justify-between items-center">
-      <div className="flex items-center gap-3 flex-1">
-        
-      <Avatar className='cursor-pointer'>
-                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className="w-full relative">
-            <Textarea className="" placeholder="Enter your comment" />
-            <div className="absolute bottom-6  right-4">
-            <SendIcon className=""/> 
-            </div>
-            </div>
-            
-      </div>
-      <div className='flex w-[12rem]  justify-around text-black mt-5'>
-          <div className='flex items-center gap-1'>
-          <Heart className="font-bold text-primary text-xl "/><span className='text-[0.8rem] text-primary font-bold'>3</span> 
-          
-          </div>
-          <div className='flex items-center gap-1'>
-          <LucideMessageSquare className=""/> <span className='text-[0.8rem]'>0</span> 
-        </div>
-        <div className='flex items-center gap-1'>
-          <Share/><span className='text-[0.8rem]'>0</span> 
-        </div>
-       
-        
-        </div>
-    </div>
-  </div>
-  <div className="bg-white px-12 py-12 mb-6 rounded-3xl">
-    <div className="flex justify-between">
-    <div className="flex justify-between w-[5rem] gap-3 ">
-        <Avatar className='cursor-pointer'>
-                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-    
-            <div className="flex flex-col">
-              <h2 className="font-bold text-md " style={{ color:"black" }}>#uizog234</h2>
-              <span className="text-sm text-gray-500 ">1h ago</span>
-            </div>
-        </div>
-        <div className="">
-       <MoreHorizontal/>
-        </div>
-    </div>
-  
-  <div className="mt-5 h-96 flex justify-center items-center  p-10 px-8 rounded-2xl " style={{ background:"linear-gradient(to top left,#40E0D0,#FF8C00,#FF0080)" }}>
-        
-        <div className="mt-5">
-          <h2 className="text-3xl font-bold" style={{ color:"white" }}>-Hello i like this social media.it{`'`} very beautiful post...</h2>
-        </div>
-        
-    </div>
-    
-    <div className="flex mt-5 justify-between items-center">
-      <div className="flex items-center gap-3 flex-1">
-        
-      <Avatar className='cursor-pointer'>
-                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className="w-full relative">
-            <Textarea className="" placeholder="Enter your comment" />
-            <div className="absolute bottom-6  right-4">
-            <SendIcon className=""/> 
-            
-            </div>
-            </div>
-            
-      </div>
-      <div className='flex w-[12rem]  justify-around text-black mt-5'>
-          <div className='flex items-center gap-1'>
-          <Heart className="font-bold text-primary text-xl "/><span className='text-[0.8rem] text-primary font-bold'>3</span> 
-          
-          </div>
-          <div className='flex items-center gap-1'>
-          
-          <LucideMessageSquare className=""/> <span className='text-[0.8rem]'>0</span> 
-        </div>
-        <div className='flex items-center gap-1'>
-
-          <Share/><span className='text-[0.8rem]'>0</span> 
-        </div>
-       
-        
-        </div>
-    </div>
-  </div>
-</div>
+<PostList/>
 
 </section>
 
--- components/posts/postList.tsx
+++ components/posts/postList.tsx
@@ -1,37 +1,128 @@
-
-import fetchPostCurrentUser from '@/db/queries/posts/fetchPostCurrentUser'
-import { LucideMessageSquare, ThumbsDown, ThumbsUp } from 'lucide-react'
-import Link from 'next/link'
+import { Heart, LucideMessageSquare, MoreHorizontal, SendIcon, Share } from 'lucide-react'
 import React from 'react'
+import { Textarea } from '../ui/textarea'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
-export default async function PostList() {
-    const posts=await fetchPostCurrentUser()
-    
+export default function PostList() {
   return (
-    <div className='grid grid-cols-3 gap-5'>
-        {posts.map((post)=><div key={post.id}>
-                <Link href={`/posts/${post.id}`}>
+    <div className="mt-5 flex flex-col gap-3">
+  <div className="bg-white px-12 py-12 mb-6 rounded-3xl">
+    <div className="flex justify-between">
+    <div className="flex justify-between w-[5rem] gap-3 ">
+        <Avatar className='cursor-pointer'>
+                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
     
-    <div className='flex flex-col'>
-    <div style={{ background:`${post.bgColor}` }} className={`relative h-[200px]  flex items-center justify-center rounded-lg cursor-pointer  hover:text-black hover:bg-[#00000013] duration-150  border-[#0000002f] border-[0.12rem]`}>
-    <p className='text-center text-white p-3'>{post.content}</p>
+            <div className="flex flex-col">
+              <h2 className="font-bold text-md " style={{ color:"black" }}>#uizog234</h2>
+              <span className="text-sm text-gray-500 ">1h ago</span>
+            </div>
+        </div>
+        <div className="">
+       <MoreHorizontal/>
+        </div>
     </div>
-    <div className='flex mt-3 justify-between'>
-      <div className='flex'>
-      <LucideMessageSquare/> <span className='text-[0.8rem]'>0</span> 
+  
+  <div className="mt-5 h-96 flex justify-center items-center  p-10 px-8 rounded-2xl " style={{ background:"linear-gradient(to top left,#000000,#434343)" }}>
+        
+        <div className="mt-5">
+          <h2 className="text-3xl font-bold" style={{ color:"white" }}>-How create post in Hide ,Please..I have forget that...</h2>
+        </div>
+        
+    </div>
+    
+    <div className="flex mt-5 justify-between items-center">
+      <div className="flex items-center gap-3 flex-1">
+        
+      <Avatar className='cursor-pointer'>
+                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="w-full relative">
+            <Textarea className="" placeholder="Enter your comment" />
+            <div className="absolute bottom-6  right-4">
+            <SendIcon className=""/> 
+            </div>
+            </div>
+            
       </div>
-    <div className='flex items-center'>
-      <ThumbsDown/><span className='text-[0.8rem]'>0</span> 
+      <div className='flex w-[12rem]  justify-around text-black mt-5'>
+          <div className='flex items-center gap-1'>
+          <Heart className="font-bold text-primary text-xl "/><span className='text-[0.8rem] text-primary font-bold'>3</span> 
+          
+          </div>
+          <div className='flex items-center gap-1'>
+          <LucideMessageSquare className=""/> <span className='text-[0.8rem]'>0</span> 
+        </div>
+        <div className='flex items-center gap-1'>
+          <Share/><span className='text-[0.8rem]'>0</span> 
+        </div>
+       
+        
+        </div>
     </div>
-    <div className='flex'>
-      <ThumbsUp/><span className='text-[0.8rem]'>0</span> 
+  </div>
+  <div className="bg-white px-12 py-12 mb-6 rounded-3xl">
+    <div className="flex justify-between">
+    <div className="flex justify-between w-[5rem] gap-3 ">
+        <Avatar className='cursor-pointer'>
+                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+    
+            <div className="flex flex-col">
+              <h2 className="font-bold text-md " style={{ color:"black" }}>#uizog234</h2>
+              <span className="text-sm text-gray-500 ">1h ago</span>
+            </div>
+        </div>
+        <div className="">
+       <MoreHorizontal/>
+        </div>
     </div>
+  
+  <div className="mt-5 h-96 flex justify-center items-center  p-10 px-8 rounded-2xl " style={{ background:"linear-gradient(to top left,#40E0D0,#FF8C00,#FF0080)" }}>
+        
+        <div className="mt-5">
+          <h2 className="text-3xl font-bold" style={{ color:"white" }}>-Hello i like this social media.it{`'`} very beautiful post...</h2>
+        </div>
+        
     </div>
     
-    </div>
-    </Link>
-            </div>)}
+    <div className="flex mt-5 justify-between items-center">
+      <div className="flex items-center gap-3 flex-1">
+        
+      <Avatar className='cursor-pointer'>
+                    <AvatarImage src="/hidd.jpg" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="w-full relative">
+            <Textarea className="" placeholder="Enter your comment" />
+            <div className="absolute bottom-6  right-4">
+            <SendIcon className=""/> 
+            
+            </div>
+            </div>
+            
+      </div>
+      <div className='flex w-[12rem]  justify-around text-black mt-5'>
+          <div className='flex items-center gap-1'>
+          <Heart className="font-bold text-primary text-xl "/><span className='text-[0.8rem] text-primary font-bold'>3</span> 
+          
+          </div>
+          <div className='flex items-center gap-1'>
+          
+          <LucideMessageSquare className=""/> <span className='text-[0.8rem]'>0</span> 
+        </div>
+        <div className='flex items-center gap-1'>
 
+          <Share/><span className='text-[0.8rem]'>0</span> 
+        </div>
+       
+        
+        </div>
+    </div>
+  </div>
 </div>
   )
 }
