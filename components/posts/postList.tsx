d84ecfea767c0fd9bd2b1312dc1bdc35fcdfdb13
level237 <Bramslevel129@gmail.com>
2024-07-10 13:34:44 +0100

    post with image

--- components/posts/postList.tsx
+++ components/posts/postList.tsx
@@ -65,6 +65,62 @@ export default function PostList() {
         </div>
     </div>
   </div>
+  <div className="bg-[#363636] px-6 py-4 mb-6 rounded-3xl">
+    <div className="flex justify-between">
+    <div className="flex justify-between w-[8rem] ">
+      <div className='flex-1'>
+      <Avatar style={{ background:"url('/profile2.jpg')",backgroundPosition:"center",backgroundSize:"cover" }} className='cursor-pointer w-10 h-10 rounded-xl'>
+                    
+                   
+                    </Avatar>
+      </div>
+    
+    
+            <div className="flex  flex-col flex-2">
+              <h2 className="font-bold text-sm text-gray-400" >Zed Camara</h2>
+              <span className="text-sm text-gray-500 ">1h ago</span>
+            </div>
+        </div>
+        <div className="text-white">
+       <MoreHorizontal/>
+        </div>
+    </div>
+  
+  <div className="mt-5 h-96 flex justify-center items-center  p-10 px-8 rounded-2xl " style={{ background:"url('/cover.png')",backgroundPosition:"center",backgroundSize:"cover",backgroundRepeat: "no-repeat" }}>
+        
+       
+        
+    </div>
+    
+    <div className="flex mt-5 justify-between items-center">
+      <div className="flex items-center gap-3 flex-1">
+        
+      <Avatar style={{ background:"url('/profile.jpg')",backgroundPosition:"center",backgroundSize:"cover" }} className='cursor-pointer w-8 h-8 rounded-xl'>
+            </Avatar>
+            <div className="w-full relative">
+            <textarea name="" id="" placeholder='Enter your comment' className={`font-bold placeholder:text-sm  w-full bg-[#282828] resize-none max-h-12 flex justify-center relative overflow-y-hidden border-[#00000041] rounded-2xl placeholder:text-[#ffffff21] py-4 px-3 cursor-pointer        text-white  text-sm`} ></textarea>
+            <div className="absolute bottom-2  right-4">
+            <SendIcon className="w-6 h-6 text-gray-400"/> 
+            </div>
+            </div>
+            
+      </div>
+      <div className='flex w-[12rem]  justify-around text-black mt-5'>
+          <div className='flex items-center gap-1'>
+          <Heart className="font-bold text-primary fill-current text-md w-6 h-6"/><span className='text-[0.8rem] text-primary font-bold'>32</span> 
+          
+          </div>
+          <div className='flex items-center gap-1 text-gray-300'>
+          <LucideMessageSquare className="w-6 h-6"/> <span className='text-[0.8rem]'>0</span> 
+        </div>
+        <div className='flex items-center gap-1 text-gray-300'>
+          <Share className='w-6 h-6'/><span className='text-[0.8rem]'>0</span> 
+        </div>
+       
+        
+        </div>
+    </div>
+  </div>
   <div className="bg-[#363636] px-6 py-4  rounded-3xl">
     <div className="flex justify-between">
     <div className="flex justify-between w-[12rem] ">
